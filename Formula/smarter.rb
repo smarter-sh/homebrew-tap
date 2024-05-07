5cc50777996077682ae109145c232b556bd133fe
lpm0073 <lpm0073@gmail.com>
2024-05-07 09:23:08 -0600

    chore: fix homebrew lint errors

--- Formula/smarter.rb
+++ Formula/smarter.rb
@@ -1,6 +1,5 @@
 class Smarter < Formula
-  desc "A multi-platform command-line interface for working with Smarter resources."
-  version "0.0.1"
+  desc "Command-line interface for working with Smarter resources."
   homepage "https://smarter.sh/"
   url "https://github.com/QueriumCorp/smarter-cli/releases/download/v0.0.1/smarter-macos-latest-0.0.1"
   sha256 "47f3c5eee50922651dd2617cf7a462625f2fadad860ad55bc12d27a67b1e8a0f"
